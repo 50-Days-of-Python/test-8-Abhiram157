234c76d27816a0018a5eda9dde64b028ad6f7f99
Abhiram157 <105275404+Abhiram157@users.noreply.github.com>
2023-01-11 12:19:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 '''Finding the largest of 2 numbers'''
 
-def highest_of(a,b):
+def highest_of():
   ans = 0
   #Write your code here.
+  a=int(input())
+  b=int(input())
+  ans=a+b
   
   
   return ans
